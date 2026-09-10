aa41ead91acb46819496ce2a72ee836f849f82c6
Raihan Mursalin Ratul <raihanmursalinratul@gmail.com>
2026-09-11 00:46:25 +0600

    Time: 76 ms (88.79%) | Memory: 32 MB (41.51%) - LeetSync

---
+++ 155-min-stack/min-stack.py
@@ -0,0 +1,27 @@
+class MinStack:
+
+    def __init__(self) :
+      self.stack=[]
+      self.min_stack=[]
+
+    def push(self, val: int):
+      self.stack.append(val)
+      if not self.min_stack:
+        self.min_stack.append(val)
+      else:
+        current_min=min(val,self.min_stack[-1])
+        self.min_stack.append(current_min)
+       
+          
+
+    def pop(self) :
+      self.stack.pop()
+      self.min_stack.pop()
+        
+
+    def top(self) :
+      return self.stack[-1]
+       
+
+    def getMin(self) :
+      return self.min_stack[-1]
